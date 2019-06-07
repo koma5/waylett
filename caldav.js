36f62a444b44dea0444e8005b1416e9a1609676f
Marco Koch <marco@5th.ch>
2019-06-07 21:05:27 +0100

    hooked up simple caldav event nextcloud poster to an xmpp interface; jabbering my notifications now

--- caldav.js
+++ caldav.js
@@ -1,6 +1,7 @@
 var chrono = require('chrono-node');
 var moment = require('moment');
 var uuidv1 = require('uuid/v1');
+var xmpp = require('simple-xmpp');
 
 const env = process.env;
 
@@ -12,6 +13,11 @@ var caldav = require('@datafire/caldav').create({
     principalPath: "/principals"
 });
 
+var myMaster = env.MASTER
+var jid = env.JABBERID
+var pwd = env.JABBERPASSWORD
+var server = env.JABBERSERVER
+var port = 5222;
 
 /*
 caldav.listCalendars({}).then(data => {
@@ -25,28 +31,57 @@ caldav.listEvents({ "filename": "/calendars/" + env.USER + "/" + env.CALENDER
 });
 */
 
-var text = process.argv[2]
-var parseResults = chrono.parse(text)
-var eventFilename = uuidv1();
+function createEventFrom(text) {
 
-if (parseResults.length > 0) {
-    var eventName =  text.replace(parseResults[0].text, '').trim();
-    var startDate = parseResults[0].start.date();
-    try {
-        var endDate = parseResults[0].end.date();
+    var parseResults = chrono.parse(text)
+    var eventFilename = uuidv1();
+
+    if (parseResults.length > 0) {
+        var eventName =  text.replace(parseResults[0].text, '').trim();
+        var startDate = parseResults[0].start.date();
+        try {
+            var endDate = parseResults[0].end.date();
+        }
+        catch(error){
+            var endDate = startDate;
+        }
     }
-    catch(error){
-        var endDate = startDate;
-    }
+
+
+    caldav.createEvent({
+        start: startDate.toString(),
+        end: endDate.toString(),
+        summary: eventName,
+        filename: "/calendars/" + env.USER + "/" + env.CALENDER + "/" + eventFilename
+    }).then(data => {console.log(data)});
+    
+    return [eventFilename, eventName, startDate, endDate].join(' ')
+
 }
 
-console.log(eventFilename, eventName, startDate, endDate)
-
-caldav.createEvent({
-    start: startDate.toString(),
-    end: endDate.toString(),
-    summary: eventName,
-    filename: "/calendars/" + env.USER + "/" + env.CALENDER + "/" + eventFilename
-}).then(data => {console.log(data)});
 
 
+xmpp.on('online', function(data) {
+        console.log('Connected with JID: ' + data.jid.user);
+});
+
+xmpp.on('error', function(err) {
+        console.error("error:", JSON.stringify(err));
+});
+
+xmpp.on('chat', function(from, message) {
+        console.log(from, message)
+        if (from === myMaster) {
+            xmpp.send(from, createEventFrom(message));
+        }
+        else {
+            xmpp.send(from, "sorry. You are not my master!");
+        }
+});
+
+xmpp.connect({
+        jid: jid,
+        password: pwd,
+        host: server,
+        port: port
+});
