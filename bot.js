koma5/waylett

commit 93ea5e1f28e3671aa2bee900e03d90d33c8b8122

configure timezone and set timezone via environment variable

bot.js
@@ -8,6 +8,10 @@ var schedule = require('node-schedule')
 
 const env = process.env;
 
+var timezone = env.TIMEZONE;
+
+moment.tz.setDefault(timezone);
+
 var myMaster = env.MASTER
 var jid = env.JABBERID
 var pwd = env.JABBERPASSWORD
@@ -26,9 +30,9 @@ function createEventFrom(text, callback) {
 
     if (parseResults.length > 0) {
         var eventName =  text.replace(parseResults[0].text, '').trim();
-        var startDate = parseResults[0].start.date();
+        var startDate = moment(parseResults[0].start.date());
         try {
-            var endDate = parseResults[0].end.date();
+            var endDate = moment(parseResults[0].end.date());
             var summary = [startDate, '\n', endDate].join(' ')
         }
         catch(error){
@@ -44,7 +48,7 @@ function createEventFrom(text, callback) {
             startDate: startDate,
             endDate: endDate,
             allDayEvent: false,
-            tzid: "Europe/London",
+            tzid: timezone,
             summary: eventName,
             key: eventFilename
             },

commit c06993bcec2ababf65d1b5c2c86a2ce1a60d3f01

fixed bug introduced in 93ea5e1f28e3671aa2bee900e03d90d33c8b8122

bot.js
@@ -8,7 +8,8 @@ var schedule = require('node-schedule')
 
 const env = process.env;
 
-var timezone = env.TIMEZONE;
+var timezone = env.WAYLETTTIMEZONE;
+console.log(timezone)
 
 moment.tz.setDefault(timezone);
 
@@ -128,7 +129,7 @@ xmpp.getRoster();
 
 function scheduleSingleEvent(startDate, name) {
     var job = schedule.scheduleJob(
-        startDate, () => {
+        startDate.toDate(), () => {
             var message = name ? name : "Beschaaaid!"
             send(message)
         });
@@ -141,7 +142,7 @@ function scheduleEvents(scheduleFreq) {
         var eventList = [];
         log("scheduling, scheduling every " + scheduleFreq.count + " " + scheduleFreq.period)
         events.forEach((i) => {
-            var scheduleDate = moment(i.startDate.toString()).tz(i.startDate.timezone).toDate()
+            var scheduleDate = moment(i.startDate.toString()).tz(i.startDate.timezone)
             scheduleSingleEvent(scheduleDate, i.summary);
         })
 